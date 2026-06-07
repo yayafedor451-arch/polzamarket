fe0998cd1d6726a1e6f0eaa68b68c4b0c074df2f
yayafedor451-arch <yayafedor451@gmail.com>
2026-06-07 12:37:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- НАСТРОЙКИ ---
-    const API_BASE_URL = 'https://polzamarket.shop';
+    const API_BASE_URL = 'https://polzamarket.shop:443';
     const tg = window.Telegram.WebApp;
 
     // --- ГЛОБАЛЬНОЕ СОСТОЯНИЕ ---
